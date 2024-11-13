3977f5c4f4632ad77d1db1161c21f6cf29cf55d6
Luca-Philipp Grumbach <xxlpmghdxx@gmail.com>
2024-11-13 22:10:21 +0100

    added kubernetes config

---
+++ submitCluster.sh
@@ -0,0 +1,5 @@
+spark-submit \
+  --class me.lpmg.ste.Main \
+  --properties-file spark-defaults.conf \
+  target/scala-2.12/spark-trustworthiness-estimator_2.12-0.1.0.jar \
+  /Users/lpmg/Documents/xml
