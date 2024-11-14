42cb288905b0a82cef936a0bd101fb8f11bc8397
Luca-Philipp Grumbach <xxlpmghdxx@gmail.com>
2024-11-14 09:57:47 +0100

    updated config file

--- submitCluster.sh
+++ submitCluster.sh
@@ -2,4 +2,4 @@ spark-submit \
   --class me.lpmg.ste.Main \
   --properties-file spark-defaults.conf \
   target/scala-2.12/spark-trustworthiness-estimator_2.12-0.1.0.jar \
-  /Users/lpmg/Documents/xml
+  /mnt/ceph/storage/corpora/corpora-thirdparty/corpus-wikipedia/wikimedia-history-snapshots/enwiki-20220901/
