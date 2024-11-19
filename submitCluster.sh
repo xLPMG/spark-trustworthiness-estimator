0a72b7e032b7476a45b7b10cb7ffd4151d100eec
Luca-Philipp Grumbach <xxlpmghdxx@gmail.com>
2024-11-19 14:09:32 +0100

    remove docker for now

--- submitCluster.sh
+++ submitCluster.sh
@@ -3,6 +3,7 @@
 spark-submit \
   --class me.lpmg.ste.Main \
   --name Spark-Trustworthiness-Estimator \
+  --properties-file spark-defaults.conf \
   --deploy-mode cluster \
   --packages com.github.tototoshi:scala-csv_2.12:2.0.0,com.typesafe.scala-logging:scala-logging_2.12:3.9.5 \
   target/scala-2.12/spark-trustworthiness-estimator_2.12-0.1.0.jar \
