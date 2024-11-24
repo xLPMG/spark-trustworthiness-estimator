e78a3a458378f722b37f11265543e5813defb438
Luca-Philipp Grumbach <xxlpmghdxx@gmail.com>
2024-11-24 22:43:54 +0100

    rework: RevisionSAXHandler

--- src/main/scala/me/lpmg/ste/data/RevisionSAXHandler.scala
+++ src/main/scala/me/lpmg/ste/data/RevisionSAXHandler.scala
@@ -13,13 +13,12 @@ class RevisionSAXHandler extends DefaultHandler {
 
   private var dictionary: Map[String, (Long, String)] = Map.empty
 
-  private var currentElement: String = ""
   private var insidePage = false
   private var insideRevision = false
   private var isMainNamespace = false
 
+  private var pageTitle: Option[String] = None
   private var pageId: Long = 0
-  private var currentPageTitle: Option[String] = None
   private var revisionId: Long = 0
   private var parentId: Option[Long] = None
   private var timestamp: Long = 0
@@ -35,13 +34,11 @@ class RevisionSAXHandler extends DefaultHandler {
       qName: String,
       attributes: Attributes
   ): Unit = {
-    currentElement = qName
     charBuffer.clear()
 
     qName match {
       case "page" =>
         insidePage = true
-        currentPageTitle = None
         isMainNamespace = false // reset
       case "revision" =>
         insideRevision = true
@@ -59,64 +56,80 @@ class RevisionSAXHandler extends DefaultHandler {
       qName: String
   ): Unit = {
     if (insideRevision) {
-      currentElement match {
-        case "id" if revisionId == 0 =>
-          revisionId = charBuffer.toString.trim.toLong
-        case "parentid" =>
-          parentId = Some(charBuffer.toString.trim.toLong)
-        case "timestamp" =>
-          try {
-            timestamp = Instant.parse(charBuffer.toString.trim).toEpochMilli()
-          } catch {
-            case e: Exception =>
-              println(
-                s"Error parsing timestamp: ${charBuffer.toString.trim} for revision: $revisionId in page: $pageId"
-              )
-          }
-        case "text" =>
-          val content = charBuffer.toString.trim
-          isRedirect = content.startsWith("#REDIRECT")
-          if (!isRedirect) {
-            /* As soon as the links are found, they are resolved to page IDs.
-             * While that might seem impractical, it was absolutely necessary
-             * because storing all the strings required an extremely large
-             * amount of memory.
-             */
-            val fullLinks = outlinkPattern
-              .findAllMatchIn(content)
-              .map(_.group(1))
-              .filter(Filter.isArticleTitle)
-              .toSet
-            val pageTitles = fullLinks.flatMap { link =>
-              val parts = link.split("\\|")
-              if (parts.nonEmpty && parts.head.nonEmpty) Some(parts.head)
-              else None
-            }.toSet
-            outlinkPageIds =
-              LinkResolver.resolvePageTitlesToPageIDs(pageTitles, dictionary)
-          } else if (currentPageTitle.isDefined) {
-            // for redirects, we resolve the redirect target and store it as the only outlink
-            val redirectTarget =
-              LinkResolver.resolveRedirect(currentPageTitle.get, dictionary)
-            if (redirectTarget != -1) {
-              outlinkPageIds = Set(redirectTarget)
-            }
-          }
-        case _ => // No-op for other elements
-      }
+      // revision specific data
+      handleRevision(qName)
     } else if (insidePage && !insideRevision) {
-      currentElement match {
-        case "title" => currentPageTitle = Some(charBuffer.toString.trim)
-        case _       => // No-op for other elements
+      // page specific data
+      qName match {
+        case "title" => pageTitle = Some(charBuffer.toString.trim)
+        case "page"  => insidePage = false
+        case "ns" =>
+          if ("0".equals(charBuffer.toString.trim)) isMainNamespace = true
+        case "id" => pageId = charBuffer.toString.trim.toLong
+        case _    => // No-op for other elements
       }
     }
+  }
 
-    qName match {
-      case "page" =>
-        insidePage = false
+  override def characters(
+      ch: Array[Char],
+      start: Int,
+      length: Int
+  ): Unit = {
+    charBuffer.appendAll(ch, start, length)
+  }
+
+  /** Handles the revision elements in the XML.
+    *
+    * @param currentElement
+    */
+  private def handleRevision(currentElement: String): Unit = {
+    currentElement match {
+      case "id" if revisionId == 0 =>
+        revisionId = charBuffer.toString.trim.toLong
+      case "parentid" =>
+        parentId = Some(charBuffer.toString.trim.toLong)
+      case "timestamp" =>
+        try {
+          timestamp = Instant.parse(charBuffer.toString.trim).toEpochMilli()
+        } catch {
+          case e: Exception =>
+            println(
+              s"Error parsing timestamp: ${charBuffer.toString.trim} for revision: $revisionId in page: $pageId"
+            )
+        }
+      case "text" =>
+        val content = charBuffer.toString.trim
+        isRedirect = content.startsWith("#REDIRECT")
+        if (!isRedirect) {
+          /* As soon as the links are found, they are resolved to page IDs.
+           * While that might seem impractical, it was absolutely necessary
+           * because storing all the strings required an extremely large
+           * amount of memory.
+           */
+          val fullLinks = outlinkPattern
+            .findAllMatchIn(content)
+            .map(_.group(1))
+            .filter(Filter.isArticleTitle)
+            .toSet
+          val pageTitles = fullLinks.flatMap { link =>
+            val parts = link.split("\\|")
+            if (parts.nonEmpty && parts.head.nonEmpty) Some(parts.head)
+            else None
+          }.toSet
+          outlinkPageIds =
+            LinkResolver.resolvePageTitlesToPageIDs(pageTitles, dictionary)
+        } else if (pageTitle.isDefined) {
+          // for redirects, we resolve the redirect target and store it as the only outlink
+          val redirectTarget =
+            LinkResolver.resolveRedirect(pageTitle.get, dictionary)
+          if (redirectTarget != -1) {
+            outlinkPageIds = Set(redirectTarget)
+          }
+        }
       case "revision" =>
+        // only add the revision if it is in the main namespace
         if (insidePage && isMainNamespace) {
-          // Add the extracted revision data to the list of revisions
           revisions += new Revision(
             revisionId,
             pageId,
@@ -130,27 +143,7 @@ class RevisionSAXHandler extends DefaultHandler {
           )
         }
         insideRevision = false
-      case _ => // No-op for other tags
-    }
-    currentElement = ""
-  }
-
-  override def characters(
-      ch: Array[Char],
-      start: Int,
-      length: Int
-  ): Unit = {
-    charBuffer.appendAll(ch, start, length)
-
-    if (
-      insidePage && currentElement == "ns" && charBuffer.toString.trim == "0"
-    ) {
-      isMainNamespace = true
-    }
-
-    if (insidePage && !insideRevision && currentElement == "id") {
-      pageId =
-        charBuffer.toString.trim.toLong // Page ID only needs to be set once per page
+      case _ => // No-op for other elements
     }
   }
 
--- submitLocally.sh
+++ submitLocally.sh
@@ -2,7 +2,9 @@ spark-submit \
   --class me.lpmg.ste.Main \
   --name Spark-Trustworthiness-Estimator \
   --master local[10] \
-  --driver-memory 28g \
+  --driver-memory 26g \
+  --conf spark.driver.bindAddress=127.0.0.1 \
+  --conf spark.driver.host=127.0.0.1 \
   --packages com.github.tototoshi:scala-csv_2.12:2.0.0,com.typesafe.scala-logging:scala-logging_2.12:3.9.5 \
   target/scala-2.12/spark-trustworthiness-estimator_2.12-0.1.0.jar \
   /Users/lpmg/Documents/xml \
