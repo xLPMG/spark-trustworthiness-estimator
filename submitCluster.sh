2ebe8c2d5e516b101080ee6b5a2e805100992dc0
Luca-Philipp Grumbach <xxlpmghdxx@gmail.com>
2024-12-08 16:52:56 +0100

    external jars

--- submitCluster.sh
+++ submitCluster.sh
@@ -6,7 +6,8 @@ spark-submit \
   --properties-file spark-defaults.conf \
   --deploy-mode cluster \
   --verbose \
-  --packages com.typesafe.scala-logging:scala-logging_2.12:3.9.5,\
+  --conf "spark.driver.extraClassPath=/mnt/ceph/storage/data-tmp/current/li83keq/jars/*" \
+  --conf "spark.executor.extraClassPath=/mnt/ceph/storage/data-tmp/current/li83keq/jars/*" \
   target/scala-2.12/spark-trustworthiness-estimator_2.12-0.1.0.jar \
   /mnt/ceph/storage/corpora/corpora-thirdparty/corpus-wikipedia/wikimedia-history-snapshots/enwiki-20220901/ \
   /mnt/ceph/storage/data-tmp/current/li83keq/ste-data/
