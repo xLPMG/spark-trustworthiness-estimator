ac9b4a00cb200e0e1409d659b4425c29dc1cfc28
Luca-Philipp Grumbach <xxlpmghdxx@gmail.com>
2024-12-08 18:26:48 +0100

    cleanup

--- submitCluster.sh
+++ submitCluster.sh
@@ -5,6 +5,7 @@ spark-submit \
   --name Spark-Trustworthiness-Estimator \
   --properties-file spark-defaults.conf \
   --verbose \
+  --packages com.typesafe.scala-logging:scala-logging_2.12:3.9.5,\
   target/scala-2.12/spark-trustworthiness-estimator_2.12-0.1.0.jar \
   /mnt/ceph/storage/corpora/corpora-thirdparty/corpus-wikipedia/wikimedia-history-snapshots/enwiki-20220901/ \
   /mnt/ceph/storage/data-tmp/current/li83keq/ste-data/
