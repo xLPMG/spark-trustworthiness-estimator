bc4147eb901e66dbb8c29c3608b7490a7c01cc62
Luca-Philipp Grumbach <xxlpmghdxx@gmail.com>
2024-12-08 17:02:54 +0100

    try staging-dir

--- submitCluster.sh
+++ submitCluster.sh
@@ -4,10 +4,7 @@ spark-submit \
   --class me.lpmg.ste.Main \
   --name Spark-Trustworthiness-Estimator \
   --properties-file spark-defaults.conf \
-  --deploy-mode cluster \
   --verbose \
-  --conf "spark.driver.extraClassPath=/mnt/ceph/storage/data-tmp/current/li83keq/jars/*" \
-  --conf "spark.executor.extraClassPath=/mnt/ceph/storage/data-tmp/current/li83keq/jars/*" \
   target/scala-2.12/spark-trustworthiness-estimator_2.12-0.1.0.jar \
   /mnt/ceph/storage/corpora/corpora-thirdparty/corpus-wikipedia/wikimedia-history-snapshots/enwiki-20220901/ \
   /mnt/ceph/storage/data-tmp/current/li83keq/ste-data/
